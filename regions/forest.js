a8f05f92e9994458201820fd1a14548f8b8ec68b
Andrea RH <andrea.r.hamilton92@gmail.com>
2020-07-03 18:57:34 -0400

    removed unnecessary variables from forest.js

--- regions/forest.js
+++ regions/forest.js
@@ -9,29 +9,4 @@ const forest =
         rare: ["N/A", rPets.snapRaptor, rPets.babbledog, rPets.snipRaptor]
     };
 
-const plains = 
-    {
-        common: ["N/A", cPets.pom, cPets.woolie, cPets.cro],
-        uncommon: ["N/A", uPets.ribonnie, uPets.omp, uPets.silkyStrider, uPets.ferren],
-        rare: ["N/A", rPets.snapRaptor, rPets.stattik, rPets.hanana]
-    };
-
-const mountain = 
-    {
-        common: ["N/A", cPets.pom, cPets.gekki, cPets.bighorn, cPets.ghostfly, cPets.pyrebird],
-        uncommon: ["N/A", uPets.ribonnie, uPets.devi, uPets.chewii,],
-        rare: ["N/A", rPets.snapRaptor, rPets.kitWyvern, rPets.snakeWyvern]
-    };
-
-const coast = 
-    {
-        common: ["N/A", cPets.pom, cPets.seabun, cPets.rockGuppy],
-        uncommon: ["N/A", uPets.ribonnie, uPets.isopaw, uPets.sailfin, ],
-        rare: ["N/A", rPets.snapRaptor, rPets.gullWyvern]
-    };
-
-module.exports = 
-    forest, 
-    plains,
-    mountain,
-    coast;
+module.exports = forest;
